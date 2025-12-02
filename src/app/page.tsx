6b1925cceab5ca2910074bf82dd74f774203018f
Billsthename22 <emioluwa@icloud.com>
2025-12-02 13:29:33 +0100

    component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import Navbar from "@/app/Components/Navbar";
+import Navbar from "@/app/components/Navbar";
 type Product = {
   id: number;
   name: string;
